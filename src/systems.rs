f80b913d3c696fccfd88e92d4f35cc97a870fba0
jprekz <jprekz@users.noreply.github.com>
2018-12-23 05:41:57 +0900

    remove InputHandlerExt

--- src/systems.rs
+++ src/systems.rs
@@ -21,21 +21,6 @@ impl<N: Real> Vector2Ext<N> for Vector2<N> {
         Vector2::new(theta.cos() * r, theta.sin() * r)
     }
 }
-use std::borrow::Borrow;
-use std::hash::Hash;
-trait InputHandlerExt<T: ?Sized> {
-    fn axis_xy_value(&self, id_x: &T, id_y: &T) -> Option<Vector2<f64>>;
-}
-impl<AX, AC, T> InputHandlerExt<T> for InputHandler<AX, AC>
-where
-    AX: Hash + Eq + Clone + Send + Sync + 'static + Borrow<T>,
-    AC: Hash + Eq + Clone + Send + Sync + 'static,
-    T: Hash + Eq + ?Sized,
-{
-    fn axis_xy_value(&self, id_x: &T, id_y: &T) -> Option<Vector2<f64>> {
-        Some(Vector2::new(self.axis_value(id_x)?, self.axis_value(id_y)?))
-    }
-}
 
 pub struct PlayableSystem;
 impl<'s> System<'s> for PlayableSystem {
@@ -46,15 +31,18 @@ impl<'s> System<'s> for PlayableSystem {
     );
 
     fn run(&mut self, (input, playables, mut players): Self::SystemData) {
-        let move_vec = input.axis_xy_value("move_x", "move_y").unwrap();
-        let left_vec = input.axis_xy_value("left_x", "left_y").unwrap();
+        let axis_xy_value =
+            |x: &str, y: &str| Some(Vector2::new(input.axis_value(x)?, input.axis_value(y)?));
+
+        let move_vec = axis_xy_value("move_x", "move_y").unwrap();
+        let left_vec = axis_xy_value("left_x", "left_y").unwrap();
         let move_vec = move_vec + left_vec;
         let (move_r, move_theta) = move_vec.to_polar();
         let move_vec = Vector2::from_polar(move_r.min(1.0), move_theta);
         let move_vec = move_vec.map(|v| v as f32);
 
-        let aim_vec = input.axis_xy_value("aim_x", "aim_y").unwrap();
-        let right_vec = input.axis_xy_value("right_x", "right_y").unwrap();
+        let aim_vec = axis_xy_value("aim_x", "aim_y").unwrap();
+        let right_vec = axis_xy_value("right_x", "right_y").unwrap();
         let aim_vec = aim_vec + right_vec;
         let (aim_r, aim_theta) = aim_vec.to_polar();
         let aim_vec = Vector2::from_polar(aim_r.min(1.0), aim_theta);
