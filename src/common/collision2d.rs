ef3b7e4a0c14ee4ee941afa52d6614772d26e14f
jprekz <jprekz@users.noreply.github.com>
2019-04-03 14:48:55 +0900

    improve performance of collision calculation

--- src/common/collision2d.rs
+++ src/common/collision2d.rs
@@ -10,7 +10,7 @@ use specs_derive::Component;
 
 use crate::common::vector2ext::Vector2Ext;
 
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 
 #[derive(Component, PrefabData, Deserialize, Serialize, Clone, Debug)]
 #[prefab(Component)]
@@ -90,20 +90,23 @@ pub struct Collided {
 pub struct CollisionSystem {
     collide_entries: HashSet<(String, String)>,
     trigger_entries: HashSet<(String, String)>,
+    bitsets: HashMap<String, BitSet>,
 }
 impl CollisionSystem {
     pub fn collide(mut self, a: impl Into<String>, b: impl Into<String>) -> Self {
         let a = a.into();
         let b = b.into();
         self.collide_entries.insert((a.clone(), b.clone()));
-        self.collide_entries.insert((b, a));
+        self.bitsets.insert(a, BitSet::new());
+        self.bitsets.insert(b, BitSet::new());
         self
     }
     pub fn trigger(mut self, a: impl Into<String>, b: impl Into<String>) -> Self {
         let a = a.into();
         let b = b.into();
         self.trigger_entries.insert((a.clone(), b.clone()));
-        self.trigger_entries.insert((b, a));
+        self.bitsets.insert(a, BitSet::new());
+        self.bitsets.insert(b, BitSet::new());
         self
     }
 }
@@ -119,78 +122,128 @@ impl<'s> System<'s> for CollisionSystem {
     fn run(&mut self, system_data: Self::SystemData) {
         let (entities, colliders, mut results, mut transforms, mut rigidbodies) = system_data;
 
-        for (ent, _) in (&entities, &colliders).join() {
-            let _ = results.insert(
-                ent,
-                ColliderResult {
-                    collided: Vec::new(),
-                    collision: Vector2::zeros(),
-                },
-            );
+        for (_, bitset) in self.bitsets.iter_mut() {
+            bitset.clear();
         }
 
-        (&transforms, &colliders, &mut results).par_join().for_each(
-            |(transform_a, collider_a, result_a)| {
+        for (ent, collider) in (&entities, &colliders).join() {
+            if results.contains(ent) {
+                let result = results.get_mut(ent).unwrap();
+                result.collided.clear();
+                result.collision.fill(0.0);
+            } else {
+                let _ = results.insert(
+                    ent,
+                    ColliderResult {
+                        collided: Vec::new(),
+                        collision: Vector2::zeros(),
+                    },
+                );
+            }
+
+            self.bitsets.get_mut(&collider.tag).unwrap().add(ent.id());
+        }
+
+        let entries = self.collide_entries.union(&self.trigger_entries);
+
+        for entry in entries {
+            let (tag_a, tag_b) = entry;
+            let bitset_a = self.bitsets.get(tag_a).unwrap();
+            let bitset_b = self.bitsets.get(tag_b).unwrap();
+            let is_trigger = self.trigger_entries.contains(&entry);
+            let is_collide = self.collide_entries.contains(&entry);
+
+            for (id_a, transform_a, collider_a) in (bitset_a, &transforms, &colliders).join() {
                 let a_pos = transform_a.translation().xy();
                 let a_width = collider_a.width;
                 let a_height = collider_a.height;
-                let tag_a = collider_a.tag.clone();
 
-                for (ent_b, transform_b, collider_b) in (&entities, &transforms, &colliders).join()
-                {
+                let ent_a = entities.entity(id_a);
+
+                for (id_b, transform_b, collider_b) in (bitset_b, &transforms, &colliders).join() {
                     let b_pos = transform_b.translation().xy();
                     let b_width = collider_b.width;
                     let b_height = collider_b.height;
-                    let tag_b = collider_b.tag.clone();
 
                     let sub = b_pos - a_pos;
                     let th_x = (a_width + b_width) / 2.0;
                     let th_y = (a_height + b_height) / 2.0;
                     let sinking_x = th_x - sub.x.abs();
                     let sinking_y = th_y - sub.y.abs();
-                    if sinking_x > 0.0 && sinking_y > 0.0 {
-                        let entry = (tag_a.clone(), tag_b.clone());
 
-                        if self.trigger_entries.contains(&entry) {
-                            result_a.collided.push(Collided {
-                                entity: ent_b,
-                                tag: tag_b,
-                            });
-                        }
+                    if !(sinking_x > 0.0 && sinking_y > 0.0) {
+                        continue;
+                    }
 
-                        if !self.collide_entries.contains(&entry) {
-                            continue;
-                        }
+                    let ent_b = entities.entity(id_b);
 
+                    if is_trigger {
+                        results.get_mut(ent_a).unwrap().collided.push(Collided {
+                            entity: ent_b,
+                            tag: tag_b.clone(),
+                        });
+                        results.get_mut(ent_b).unwrap().collided.push(Collided {
+                            entity: ent_a,
+                            tag: tag_a.clone(),
+                        });
+                    }
+
+                    if is_collide {
                         if sinking_x < sinking_y {
                             if sub.x > 0.0 {
-                                if result_a.collision.x.abs() < sinking_x {
-                                    result_a.collision.x = -sinking_x;
+                                if let Some(result_a) = results.get_mut(ent_a) {
+                                    if result_a.collision.x.abs() < sinking_x {
+                                        result_a.collision.x = -sinking_x;
+                                    }
+                                }
+                                if let Some(result_b) = results.get_mut(ent_b) {
+                                    if result_b.collision.x.abs() < sinking_x {
+                                        result_b.collision.x = sinking_x;
+                                    }
                                 }
                             } else {
-                                if result_a.collision.x.abs() < sinking_x {
-                                    result_a.collision.x = sinking_x;
+                                if let Some(result_a) = results.get_mut(ent_a) {
+                                    if result_a.collision.x.abs() < sinking_x {
+                                        result_a.collision.x = sinking_x;
+                                    }
+                                }
+                                if let Some(result_b) = results.get_mut(ent_b) {
+                                    if result_b.collision.x.abs() < sinking_x {
+                                        result_b.collision.x = -sinking_x;
+                                    }
                                 }
                             }
                         } else {
                             if sub.y > 0.0 {
-                                if result_a.collision.y.abs() < sinking_y {
-                                    result_a.collision.y = -sinking_y;
+                                if let Some(result_a) = results.get_mut(ent_a) {
+                                    if result_a.collision.y.abs() < sinking_y {
+                                        result_a.collision.y = -sinking_y;
+                                    }
+                                }
+                                if let Some(result_b) = results.get_mut(ent_b) {
+                                    if result_b.collision.y.abs() < sinking_y {
+                                        result_b.collision.y = sinking_y;
+                                    }
                                 }
                             } else {
-                                if result_a.collision.y.abs() < sinking_y {
-                                    result_a.collision.y = sinking_y;
+                                if let Some(result_a) = results.get_mut(ent_a) {
+                                    if result_a.collision.y.abs() < sinking_y {
+                                        result_a.collision.y = sinking_y;
+                                    }
+                                }
+                                if let Some(result_b) = results.get_mut(ent_b) {
+                                    if result_b.collision.y.abs() < sinking_y {
+                                        result_b.collision.y = -sinking_y;
+                                    }
                                 }
                             }
                         }
                     }
                 }
-            },
-        );
+            }
+        }
 
-        for (result, transform, rigidbody) in
-            (&mut results, &mut transforms, &mut rigidbodies).join()
-        {
+        for (result, transform, rigidbody) in (&results, &mut transforms, &mut rigidbodies).join() {
             if result.collision != Vector2::zeros() {
                 let normal = result.collision.normalize();
                 let bounciness = rigidbody.bounciness;
